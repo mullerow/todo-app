bf183df7104d9cbd877c23af2c7dcdc1bc27d66b
mullerow <maximilian.kohler@posteo.de>
2024-03-08 22:39:45 +0100

    feat: check avoids now duplicates

--- script.js
+++ script.js
@@ -8,15 +8,34 @@ let todos = [
   { description: "2. todo des Tages", id: 2, done: true },
 ];
 function addTodo(event) {
-  const freshTodo = {
-    description: input.value,
-    id: todos.length + 1,
-    done: false,
-  };
-  todos.push(freshTodo);
-  const storageList = JSON.stringify(todos);
-  localStorage.setItem("todoList", storageList);
-  renderTodos();
+  let duplicate = false;
+  // wenn das das array durch JSON auf null gesetzt wurde, dann ändere bitte todos in array
+  if (todos === null) {
+    todos = [];
+  }
+  // ausschließen das ein leerer string als todo übergeben wird
+  if (input.value.trim() === "") {
+    return;
+  }
+  // suche nach duplikaten
+  todos.forEach((todo) => {
+    duplicate = todo.description.includes(input.value);
+    console.log("duplicate", duplicate);
+  });
+  // wenn ein duplikat gefunden wurde, breche das hinzufügen eines todos ab
+  if (duplicate === true) {
+    return;
+  } else {
+    const freshTodo = {
+      description: input.value,
+      id: todos.length + 1,
+      done: false,
+    };
+    todos.push(freshTodo);
+    const storageList = JSON.stringify(todos);
+    localStorage.setItem("todoList", storageList);
+    renderTodos();
+  }
 }
 
 FormContainers.forEach((form) =>
@@ -27,7 +46,6 @@ FormContainers.forEach((form) =>
 
 function updateTodos(e) {
   e.target.todoObj.done = !e.target.todoObj.done;
-  console.log("todoObj", e.target.todoObj);
 }
 
 function renderTodos() {
@@ -36,25 +54,29 @@ function renderTodos() {
   // zunächst sicherheitshalber den gesammten text löschen und erst dann neu beschreiben
   todoField.innerText = "";
   // schleife durch alle Eintäge im der todo
-  todos.forEach((todo) => {
-    // erstelle ein li element
-    const listItem = document.createElement("li");
-    // erstelle eine checkbox
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = "todo-" + todo.id;
-    // erstelle ein label für die checkbox
-    const label = document.createElement("label");
-    label.htmlFor = checkbox.id;
-    label.innerText = todo.description;
-    // füge den jeweiligen item ein listeneintrag ins markdown ein
-    listItem.append(checkbox, label);
-    todoField.append(listItem);
-    checkbox.todoObj = todo;
-    if (todo.done === true) {
-      checkbox.checked = true;
-    }
-  });
+  if (todos !== null && todos.length > 0) {
+    todos.forEach((todo) => {
+      // erstelle ein li element
+      const listItem = document.createElement("li");
+      // erstelle eine checkbox
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = "todo-" + todo.id;
+      // erstelle ein label für die checkbox
+      const label = document.createElement("label");
+      label.htmlFor = checkbox.id;
+      label.innerText = todo.description;
+      // füge den jeweiligen item ein listeneintrag ins markdown ein
+      listItem.append(checkbox, label);
+      todoField.append(listItem);
+      checkbox.todoObj = todo;
+      if (todo.done === true) {
+        checkbox.checked = true;
+      }
+    });
+  } else {
+    return;
+  }
 }
 
 todoField.addEventListener("change", updateTodos);
